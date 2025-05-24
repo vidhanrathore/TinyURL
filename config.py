30634ca6aa796b939563b792d55a4374f2414409
vidhanrathore <rathorevidhan2001@gmail.com>
2025-05-24 17:34:37 +0530

    Resolved Error in config file

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ MYSQL_HOST = os.getenv('MYSQL_HOST')
 MYSQL_USER = os.getenv('MYSQL_USER')
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 MYSQL_DB = os.getenv('MYSQL_DB')
-MYSQL_PORT = int(os.getenv('MYSQL_PORT'))
+MYSQL_PORT = os.getenv('MYSQL_PORT')
 
 
 DB_CONFIG = {
